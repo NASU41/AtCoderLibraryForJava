36308c5c996402128e2a14e58bac112a2780d00f
Taranpreet Singh <47025523+taran-1407@users.noreply.github.com>
2020-10-02 01:50:41 +0530

    Updated GraphBuilder.java
    Moved example usage to separate file, modified access specifies and made methods static.

--- GraphBuilder/GraphBuilder.java
+++ GraphBuilder/GraphBuilder.java
@@ -1,5 +1,5 @@
 class GraphBuilder{
-	public int[][] makeGraph(int NumberOfNoes, int NumberOfEdges, int[] from, int[] to, boolean undirected){
+	public static int[][] makeGraph(int NumberOfNoes, int NumberOfEdges, int[] from, int[] to, boolean undirected){
 		int[][] graph = new int[NumberOfNoes][];
 		int[] outdegree = new int[NumberOfNoes];
 		for(int i = 0; i< NumberOfEdges; i++){
@@ -14,7 +14,7 @@ class GraphBuilder{
 		return graph;
 	}
 	
-	public int[][][] makeGraphWithEdgeInfo(int NumberOfNoes, int NumberOfEdges, int[] from, int[] to, boolean undirected){
+	public static int[][][] makeGraphWithEdgeInfo(int NumberOfNoes, int NumberOfEdges, int[] from, int[] to, boolean undirected){
 		int[][][] graph = new int[NumberOfNoes][][];
 		int[] outdegree = new int[NumberOfNoes];
 		for(int i = 0; i< NumberOfEdges; i++){
@@ -27,33 +27,5 @@ class GraphBuilder{
 		    if(undirected)graph[to[i]][--outdegree[to[i]]] = new int[]{from[i], i, 1};
 		}
 		return graph;
-	}
-	
-	//Usage Exmaple
-	public void makeGraphUsage(){
-	    int N = 6, E = 8;
-	    int[] from = new int[]{0,0,0,1,1,2,2,4};
-	    int[] to = new int[]{2,4,5,4,5,3,4,5};
-	    
-	    int[][] graph = makeGraph(N, E, from, to, true);
-	    for(int i = 0; i< N; i++){
-	        System.out.print(i+": ");
-	        for(int j:graph[i])System.out.print(j+" ");
-	        System.out.println();
-	    }
-	}
-	
-	//Usage Example
-	public void makeGraphWithEdgeInfoUsage(){
-	    int N = 6, E = 8;
-	    int[] from = new int[]{0,0,0,1,1,2,2,4};
-	    int[] to = new int[]{2,4,5,4,5,3,4,5};
-	    
-	    int[][][] graph = makeGraphWithEdgeInfo(N, E, from, to, true);
-	    for(int i = 0; i< N; i++){
-	        System.out.print(i+": ");
-	        for(int[] j:graph[i])System.out.print("["+j[0]+", "+j[1]+", "+j[2]+"],");
-	        System.out.println();
-	    }
 	}
 }
