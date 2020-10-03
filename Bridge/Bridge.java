0e588f8ece6df579f661f4487cb8ec39caa44d23
Taranpreet Singh <47025523+taran-1407@users.noreply.github.com>
2020-10-03 11:40:03 +0530

    Updated Bridge.java
    Added support for multi-edges and self loops.

--- Bridge/Bridge.java
+++ Bridge/Bridge.java
@@ -2,8 +2,14 @@ class Bridge{
 	private static int time;
 	private static int[] disc, low;
 	private static boolean[] visited;
-	
-	public static boolean[] bridges(int[][][] graphWithEdgeInfo, int EdgeCount){
+
+	public static boolean[] bridges(int[][][] graphWithEdgeInfo){
+		int edgeCount = 0;
+		for(int[][] v:graphWithEdgeInfo)edgeCount += v.length;
+		return bridges(graphWithEdgeInfo, edgeCount);
+	}
+
+	public static boolean[] bridges(int[][][] graphWithEdgeInfo, int edgeCount){
 		int NumberOfNodes = graphWithEdgeInfo.length;
 
 		time = -1;
@@ -12,23 +18,23 @@ class Bridge{
 		visited = new boolean[NumberOfNodes];
 		java.util.Arrays.fill(low, 2*NumberOfNodes);
 
-		boolean[] isBridge = new boolean[EdgeCount];
+		boolean[] isBridge = new boolean[edgeCount];
 		for(int i = 0; i< NumberOfNodes; i++)if(!visited[i] && graphWithEdgeInfo[i] != null)bridgeUtil(graphWithEdgeInfo, isBridge, i, -1);
 		return isBridge;
 	}
 
-	private static void bridgeUtil(int[][][] g, boolean[] isBridge, int u, int p){
+	private static void bridgeUtil(int[][][] g, boolean[] isBridge, int u, int parentEdge){
 		visited[u] = true;
 		disc[u] = low[u] = ++time;
 
 		for(int[] edge: g[u]){
 			int v = edge[0], edgeIndex = edge[1];
-			if(v == p)continue;
+			if(parentEdge == edgeIndex)continue;
 
 			if(visited[v])
 				low[u] = Math.min(low[u], disc[v]);
 			else{
-				bridgeUtil(g, isBridge, v, u);
+				bridgeUtil(g, isBridge, v, edgeIndex);
 				low[u] = Math.min(low[u], low[v]);
 
 				if(low[v] > disc[u])isBridge[edgeIndex] = true;
