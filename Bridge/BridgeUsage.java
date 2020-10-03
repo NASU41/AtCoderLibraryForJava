8c7ed6e952d0bb35700b2f86b96f779091be4c31
Taranpreet Singh <47025523+taran-1407@users.noreply.github.com>
2020-10-03 12:01:55 +0530

    Updated BridgeUsage.java
    Added multi-edges and self loops in example

--- Bridge/BridgeUsage.java
+++ Bridge/BridgeUsage.java
@@ -1,12 +1,12 @@
 class BridgeUsage{
 	//Usage Example Bridge.bridges()
 	public static void bridgesUsage(){
-		int N = 6, E = 6;
-		int[] from = new int[]{0,1,1,2,2,4};
-		int[] to = new int[]{5,4,5,3,4,5};
+		int N = 6, E = 9;
+		int[] from = new int[]{0,1,1,2,2,4,4,3,1};
+		int[] to = new int[]{5,4,5,3,4,5,4,2,4};
 
 		int[][][] graph = GraphBuilder.makeGraphWithEdgeInfo(N, E, from, to, true);
-		boolean[] isBridge = Bridge.bridges(graph, E);
+		boolean[] isBridge = Bridge.bridges(graph);
 
 		for(int i = 0; i< E; i++){
 			if(isBridge[i])
